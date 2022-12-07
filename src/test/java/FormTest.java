4b9b64c7175262246f9dea0ee8b86e255d0bb8bf
lst47 <linh20121@gmail.com>
2022-12-07 14:37:53 +0300

    error

---
+++ src/test/java/FormTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FormTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "./driver/mac/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestSomething() {
+        driver.get("http://localhost:9999/");
+
+    }
+
+}
