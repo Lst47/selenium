b0f208a18856b36606683dba494027514a4a5f0d
lst47 <linh20121@gmail.com>
2022-12-07 21:07:12 +0300

    Fine

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -3,9 +3,15 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FormTest {
     private WebDriver driver;
@@ -32,8 +38,17 @@ public class FormTest {
     }
 
     @Test
-    void shouldTestSomething() {
+    void shouldTestSomething() throws InterruptedException {
         driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id= name] input")).sendKeys("Василий");
+        driver.findElement(By.cssSelector("[data-test-id= phone ] input")).sendKeys("+79999999999");
+        driver.findElement(By.cssSelector("[data-test-id= agreement ]")).click();
+        driver.findElement(By.className("button_view_extra")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals(expected, actual);
+        Thread.sleep(5000);
+
 
 
     }
